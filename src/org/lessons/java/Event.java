82e3dc654a086727fc181eb0724c76570cab0593
BetterCallAle <dap22091998@gmail.com>
2023-03-17 10:58:01 +0100

    Milestone 1 completed

---
+++ src/org/lessons/java/Event.java
@@ -0,0 +1,94 @@
+package org.lessons.java;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Event {
+    String title;
+    LocalDate date;
+    int totalSeats;
+    int bookedSeats;
+
+    //CONSTRUCTOR
+    public Event(String title, LocalDate date, int totalSeats){
+        checkTitle(title);
+        checkDate(date);
+        checkTotalSeats(totalSeats);
+        this.title = title;
+        this.date = date;
+        this.totalSeats = totalSeats;
+        this.bookedSeats = 0;
+    }
+
+    //GETTERS
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public int getBookedSeats() {
+        return bookedSeats;
+    }
+
+    //SETTERS
+    public void setTitle(String title) {
+        checkTitle(title);
+        this.title = title;
+    }
+
+    public void setDate(LocalDate date) {
+        checkDate(date);
+        this.date = date;
+    }
+
+    //METHODS
+
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return formatter.format(date) + title;
+    }
+
+    private void checkTitle(String title){
+        if (title.trim().isEmpty()){
+            throw new IllegalArgumentException("Title can't be empty");
+        }
+    }
+
+    private void checkDate(LocalDate date){
+        LocalDate now = LocalDate.now();
+
+        if (now.isAfter(date)){
+            throw new IllegalArgumentException("Date can't be in the past. Your date: " + date + ", now: " + now);
+        }
+    }
+
+    private void checkTotalSeats(int seats){
+        if (seats <= 0){
+            throw new IllegalArgumentException("The total seats can't be minor than zero");
+        }
+    }
+
+    public void bookSeat(){
+        checkDate(date);
+        if (totalSeats == bookedSeats){
+            throw new IllegalArgumentException("There are not available seats");
+        }
+        bookedSeats++;
+    }
+
+    public void cancel(){
+        checkDate(date);
+        checkTotalSeats(bookedSeats);
+        bookedSeats--;
+    }
+}
--- src/org/lessons/java/Evento.java
+++
@@ -1,4 +0,0 @@
-package org.lessons.java;
-
-public class Evento {
-}
